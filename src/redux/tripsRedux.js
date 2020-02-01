44bd744723edd603d2449908d6b83079814716b8
Robert Palczewski <robert.palczewski@gmail.com>
2020-02-01 11:57:18 +0100

    Add sort by cost

--- src/redux/tripsRedux.js
+++ src/redux/tripsRedux.js
@@ -23,27 +23,32 @@ export const getFilteredTrips = ({trips, filters}) => {
   );
 
   // TODO - sort by cost descending (most expensive goes first)
-  
-
+  output = output.sort((a, b)=>{
+    const slicedA = parseFloat(a.cost.slice(1).replace(',', ''));
+    const slicedB = parseFloat(b.cost.slice(1).replace(',', ''));
+    return slicedB - slicedA;
+  });
+  console.log('output',output);
   return output;
+  
 };
 
 
 export const getTripById = ({trips}, tripId) => {
-  let filtered = trips;
+  // let filtered = trips;
 
   // TODO - filter trips by tripId - DONE
-  filtered = trips.filter(trip => trip.id === tripId);
+  const filtered = trips.filter(trip => trip.id === tripId);
 
   console.log('filtering trips by tripId:', tripId, filtered);
   return filtered.length ? filtered[0] : {error: true};
 };
 
 export const getTripsForCountry = ({trips}, countryCode) => {
-  let filtered = trips;
+  // let filtered = trips;
 
   // TODO - filter trips by countryCode - DONE
-  filtered = trips.filter(trip => trip.country.code === countryCode);
+  const filtered = trips.filter(trip => trip.country.code === countryCode);
 
   console.log('filtering trips by countryCode:', countryCode, filtered);
   return filtered.length ? filtered : [{error: true}];
