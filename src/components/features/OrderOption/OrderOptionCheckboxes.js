a04b2f097f3f70957e250db40e5c924b97598904
Robert Palczewski <robert.palczewski@gmail.com>
2020-02-10 19:34:28 +0100

    Add Number and Checkboxes options

--- src/components/features/OrderOption/OrderOptionCheckboxes.js
+++ src/components/features/OrderOption/OrderOptionCheckboxes.js
@@ -1,14 +1,45 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+import {formatPrice} from '../../../utils/formatPrice';
+import PropTypes from 'prop-types';
 
-const OrderOptionCheckboxes = () => (
-  <div>
-    OrderOptionCheckboxes
-  </div>
-);
+const newValueSet = (currentValue, id, checked) => {
+  if(checked){
+    return [
+      ...currentValue,
+      id,
+    ];
+  } else {
+    return currentValue.filter(value => value != id);
+  }
+};
+
+const OrderOptionCheckboxes = ({values, setOptionValue, currentValue}) => {
+  
+  return (
+    <div className={styles.checkboxes}>
+      {values.map(value => {
+        
+        return (
+          <label key={value.id} >
+            <input
+              type='checkbox'
+              value={value.id}
+              checked={currentValue[value.id]}
+              onChange={event => setOptionValue(newValueSet(currentValue, value.id, event.currentTarget.checked))}
+            />
+            {value.name} {formatPrice(value.price)}
+          </label>
+        );
+      })}
+    </div>
+  );
+};
 
 OrderOptionCheckboxes.propTypes = {
-
+  values: PropTypes.array,
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.array,
 };
 
 export default OrderOptionCheckboxes;
--- src/components/features/OrderOption/OrderOptionNumber.js
+++ src/components/features/OrderOption/OrderOptionNumber.js
@@ -1,14 +1,23 @@
 import React from 'react';
-// import PropTypes from 'prop-types';
+import styles from './OrderOption.scss';
+import PropTypes from 'prop-types';
+import {formatPrice} from '../../../utils/formatPrice';
 
-const OrderOptionNumber = () => (
-  <div>
-    OrderOptionNumber
-  </div>
-);
-
+const OrderOptionNumber = ({currentValue, limits, setOptionValue, price}) => {
+  console.log('OrderOptionNumber - ', 'setOptionValue: ', setOptionValue, 'currentValue: ', currentValue);
+  return (
+    <div className={styles.number}>
+      <input type='number' className={styles.inputSmall} value={currentValue} min={limits.min} max={limits.max} onChange={event => setOptionValue(event.currentTarget.value)} />
+      ({formatPrice(price)})
+    </div>
+  );
+};
+    
 OrderOptionNumber.propTypes = {
-
+  setOptionValue: PropTypes.func,
+  currentValue: PropTypes.number,
+  limits: PropTypes.object,
+  price: PropTypes.string,
 };
 
 export default OrderOptionNumber;
